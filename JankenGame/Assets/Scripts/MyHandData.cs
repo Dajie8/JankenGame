742a8649eb25c83e76a6511e3e48ef562154db07
Dajie8 <drdajie28@gmail.com>
2022-04-06 23:54:20 +0900

    不要ファイルの削除 JankenResultのGetResultをアッパーキャメルに変更 親オブジェクトにアタッチし直し

---
+++ JankenGame/Assets/Scripts/MyHandData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+[CreateAssetMenu(menuName = "playerHandData")]
+public class MyHandData : ScriptableObject
+{
+    [SerializeField] private int _playerHand = 1;
+    public int playerHand
+    {
+        get => _playerHand;
+        set => _playerHand = value;
+    }
+}
